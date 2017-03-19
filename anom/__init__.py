2e974f8060eb3f78738f1628a9547a11f8a2f65a
Bogdan Popa <popa.bogdanp@gmail.com>
2017-03-19 19:59:32 +0200

    chore: bump version

--- anom/__init__.py
+++ anom/__init__.py
@@ -4,4 +4,4 @@ from .adapter import Adapter, get_adapter, set_adapter
 from .model import Key, Model, Property, delete_multi, get_multi, put_multi, lookup_model_by_kind
 from .query import Query, Resultset, Page, Pages
 
-__version__ = "0.0.0"
+__version__ = "0.0.1"
