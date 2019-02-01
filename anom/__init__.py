1232085ab7f3e04938ecce4ce222ae0ee63d8af9
Bogdan Popa <popa.bogdanp@gmail.com>
2019-02-01 10:37:15 +0200

    chore: bump version 0.8.0 → 0.9.0

--- anom/__init__.py
+++ anom/__init__.py
@@ -6,4 +6,4 @@ from .namespaces import get_namespace, namespace, set_default_namespace, set_nam
 from .query import Query, Resultset, Page, Pages
 from .transaction import Transaction, TransactionError, RetriesExceeded, transactional
 
-__version__ = "0.8.0"
+__version__ = "0.9.0"
